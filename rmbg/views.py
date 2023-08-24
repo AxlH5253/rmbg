5b49556b19f59e77e7026cad53cc64dead3225b4
Axl Hanuebi <50287246+AxlH5253@users.noreply.github.com>
2023-08-24 20:13:28 +0900

    init rmbg

---
+++ rmbg/views.py
@@ -0,0 +1,16 @@
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from PIL import Image
+from .rmbg import remove_background
+
+@api_view(['POST'])
+def upload_image(request, format=None):
+    if(request.method == 'POST'):
+        try:
+            file_uploaded = request.FILES.get('file')
+            input_image = Image.open(file_uploaded)
+            output_image = remove_background(input_image)
+            return JsonResponse({'status': 200, 'data': output_image, 'message': 'success'}, status=200)
+        except Exception as e:
+            return JsonResponse({'status': 500, 'message': e }, status=500)
+        
